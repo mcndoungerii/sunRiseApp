1a118c212c633b1d437e8fd0adfa172b5bea5ac8
Allen <allenmcndounger@gmail.com>
2020-02-27 16:13:51 +0300

    Getting the City Name from the UI

--- app/src/main/java/com/allen/getsunset/MainActivity.kt
+++ app/src/main/java/com/allen/getsunset/MainActivity.kt
@@ -2,11 +2,18 @@ package com.allen.getsunset
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    protected fun GetSunset(view: View) {
+        var city = etCityName.text.toString();
+        val url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+ city +"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
     }
 }
